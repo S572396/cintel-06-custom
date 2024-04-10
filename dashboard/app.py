bde696a42d2fac40412ad97936fb6e0e8d6d1276
sandra ruiz <sandraozuna129@yahoo.com>
2024-04-09 22:57:41 -0500

    add scatter plot

--- dashboard/app.py
+++ dashboard/app.py
@@ -9,6 +9,9 @@ infile = r"C:\Users\19564\Documents\cintel-06-custom\cintel-06-custom\dashboard\
 # Read the CSV file into a DataFrame
 df = pd.read_csv(infile)
 
+# Calculate the total for each year and add a new column
+df['Total_Count_Per_Year'] = df.groupby('Model Year')['Model Year'].transform('count')
+
 # Define the Shiny UI Page Layout
 ui.page_opts(title="EV Count Live Data Example", fillable=True)
 
@@ -19,14 +22,12 @@ with ui.sidebar(open="open"):
     
     # Use checkbox_group to filter
     ui.input_checkbox_group(
-        "select_attribute_list",
+        "selected_attribute",
         "Select Attribute",
-        ["Make Year", "Make", "Model", "Electric_Vehicle_Type"],
-        selected=["Make Year"],
+        ["Model Year", "Make", "Model", "Electric_Vehicle_Type"],
+        selected=["Model Year"],
         inline=True
     )
-
-
 
     ui.hr()
     ui.h6("Links")
@@ -37,3 +38,16 @@ with ui.h2("Data Grid"):
     @render.data_frame
     def EV_data_grid():
         return render.DataGrid(df, height=100)
+    
+# Plotly Scatterplot
+with ui.h2("Plotly Scatterplot"):
+    @render_plotly
+    def plotly_scatterplot():
+        scatter = px.scatter(
+            df,
+            title="EV Data",
+            x=df["Model Year"],  # assuming you want to plot "Model Year" on the x-axis
+            y=df['Total_Count_Per_Year'],  # corrected the y parameter
+            color="Model Year"
+        )
+        return scatter
